bac88f7c164101999d2039865541568f9e4f9ced
Roni Laukkarinen <roni@dude.fi>
2020-02-13 17:33:56 +0200

    Add rules

---
+++ modules/rules.py
@@ -0,0 +1,10 @@
+# coding=utf-8
+"""
+rules.py
+Made by Roni Laukkarinen
+"""
+import sopel.module
+
+@sopel.module.commands('säännöt', 'saannot', 'rules', 'etiketti')
+def olenaa(bot, trigger):
+    bot.say('#pulina-kanavan säännöt, ole hyvä: https://www.pulina.fi/tietoa/#saannot')
