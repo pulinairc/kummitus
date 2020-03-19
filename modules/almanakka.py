fde299bd2ead4252417f328b2f5cc20373d19900
Roni Laukkarinen <roni.laukkarinen@gmail.com>
2020-03-19 10:46:28 +0200

    Rewrite day module

--- modules/almanakka.py
+++ modules/almanakka.py
@@ -1,29 +1,30 @@
 """
 almanakka.py - Sopel Almanakka Module
-Copyright 2015, Marcus Leivo <meicceli@sopel.mail.kapsi.fi>
+Copyright 2020, Roni "rolle" Laukkarinen <roni@dude.fi>
 Licensed under the Eiffel Forum License 2.
 http://sopel.chat/
 """
+import sopel.module
 from sopel.module import commands
 from urllib.request import urlopen
+import lxml.etree
+from lxml import etree
+import lxml.html
+import requests
 from bs4 import BeautifulSoup as BS4
 
-
-@commands(u'almanakka', u'tänään', u'nimipäivät')
+@commands(u'almanakka', u'tänään', u'nimipäivät', 'pvm')
 def almanakka(bot, trigger):
     url = "http://almanakka.helsinki.fi/fi/"
-    soossi = BS4(urlopen(url).read().decode())
-    nimet = soossi.find("div", attrs={"id": "rt-sidebar-a"})
-    nimet = nimet.find("div", attrs={"class": "module-content"})
 
-    erikoispaiva = str(nimet.find_all("p")[0].text)
-    nimet = str(nimet.find_all("p")[1])
+    # LXML Xpath based scraping
+    r = requests.get(url)
+    root = lxml.html.fromstring(r.content)
+    paiva_get = root.xpath('//*[@id="rt-sidebar-a"]/div[2]/div/div/h2')
+    nimet_get = root.xpath('//*[@id="rt-sidebar-a"]/div[2]/div/div/p[2]/text()')
+    erikoispaiva_get = root.xpath('//*[@id="rt-sidebar-a"]/div[2]/div/div/p[1]')
+    nimet = "".join(nimet_get)
+    paiva = paiva_get[0].text.strip()
+    erikoispaiva = erikoispaiva_get[0].text.strip()
 
-    nimet = BS4(str(nimet).replace("<br></br>", "").replace(")", ");"))
-    nimet = str(nimet.text.split(": ")[1][:-1])
-    output = ""
-    if erikoispaiva != "":
-        output += "%s: %s" % (erikoispaiva, nimet)
-    else:
-        output += nimet
-    bot.say(output)
+    bot.say('\x02' + paiva + '\x0F:' + erikoispaiva + '. ' + nimet + '')
