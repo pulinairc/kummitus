62550e97670e4e476ee3f9e92d17efe9f0cfaec1
Roni Laukkarinen <roni.laukkarinen@gmail.com>
2020-09-06 14:49:08 +0300

    Disable cursing warning

--- modules/triggers.py
+++ modules/triggers.py
@@ -6,6 +6,6 @@ import datetime
 import requests
 import re
 
-@sopel.module.rule(r'(^|.+)vittu(.+|$)', r'(^|.+)vitun(.+|$)', r'(^|.+)saatana(.+|$)', r'(^|.+)perkele(.+|$)', r'(^|.+)helvet(.+|$)', r'(^|.+)jumalauta(.+|$)')
-def fword(bot, trigger):
- bot.say("Älä kiroile.")
+#@sopel.module.rule(r'(^|.+)vittu(.+|$)', r'(^|.+)vitun(.+|$)', r'(^|.+)saatana(.+|$)', r'(^|.+)perkele(.+|$)', r'(^|.+)helvet(.+|$)', r'(^|.+)jumalauta(.+|$)')
+#def fword(bot, trigger):
+# bot.say("Älä kiroile.")
