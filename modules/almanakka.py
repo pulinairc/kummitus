9d5bb66f5dc9fbe333a44a37029753703024928c
Roni Laukkarinen <roni@dude.fi>
2021-05-16 15:34:37 +0300

    Fix HTTPS fail

--- modules/almanakka.py
+++ modules/almanakka.py
@@ -17,7 +17,7 @@ def almanakka(bot, trigger):
     # Get HTML page
     user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36'
     headers = {"user-agent": user_agent}
-    req = requests.get(url, headers=headers)
+    req = requests.get(url, headers=headers, verify=False)
 
     # Get stuff
     soup = BeautifulSoup(req.text, "html.parser")
