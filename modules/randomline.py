4e4e14bb3e3545b9b4d8e2ef8591c41c2dab2f21
Roni Laukkarinen <roni@dude.fi>
2021-05-19 22:38:29 +0300

    Strip extra chars

--- modules/randomline.py
+++ modules/randomline.py
@@ -12,7 +12,7 @@ import random
 @sopel.module.commands('randomjuttu')
 
 def randomline(bot, trigger):
-    files = glob.glob("/home/rolle/.sopel/training/*.json")
+    files = glob.glob("/home/rolle/.sopel/training/*.json").replace('"', '').rstrip(',')
     file = random.choice(files)
     line = random.choice(list(open(file)))
     bot.say(line)
