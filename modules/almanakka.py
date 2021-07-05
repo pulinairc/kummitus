825d3e2ce459fd229ec59f42f4ce57b008e178c0
Roni Laukkarinen <roni@dude.fi>
2021-07-05 16:19:19 +0300

    Attempt to add names natively

--- modules/almanakka.py
+++ modules/almanakka.py
@@ -10,10 +10,15 @@ from sopel.module import commands
 from bs4 import BeautifulSoup
 import requests
 import datetime
+import os
+import json
 from babel.dates import format_date, format_datetime, format_time
+
+names_file = '/home/rolle/.sopel/modules/nimipaivat.json'
 
 def scheduled_message(bot):
     url = "https://almanakka.helsinki.fi/"
+    url_accurate_names = "http://xn--nimipiv-9wac.fi/%s.%s./"   
     now = datetime.datetime.now()
 
     # Get HTML page
@@ -57,18 +62,16 @@ def run_schedule(bot):
 @commands(u'almanakka', u'tänään', u'nimipäivät', 'pvm')
 def almanakka(bot, trigger):
     
-    url = "https://almanakka.helsinki.fi/"
     now = datetime.datetime.now()
+    day = now.strftime("%dd")
+    month = now.strftime("%mm")
 
-    # Get HTML page
-    user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36'
-    headers = {"user-agent": user_agent}
-    req = requests.get(url, headers=headers, verify=False)
+    if os.path.exists(names_file):
+      filehandle = open(names_file, 'r')
+      data_json = json.loads(filehandle.read())
+      filehandle.close()
+      names = data_json[day + '-' + month]
 
-    # Get stuff
-    soup = BeautifulSoup(req.text, "html.parser")
-    day = soup.select("#rt-sidebar-a > div.rt-block.nosto > div > div > h2")
-    names = soup.select("#rt-sidebar-a > div.rt-block.nosto > div > div > p:nth-child(3)")
     findate = format_date(now, format='full', locale='fi_FI')
 
-    bot.say('Tänään on \x02' + findate + '\x0F. ' + names[0].text.strip() + '')
+    bot.say('Tänään on \x02' + findate + '\x0F. Nimipäiviään viettävät: ' + names.strip() + '')
