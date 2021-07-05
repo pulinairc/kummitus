0ae85f156773284ab4f53e31b4384def5b8518b9
Roni Laukkarinen <roni@dude.fi>
2021-07-05 15:34:51 +0300

    Simplify

--- modules/matka.py
+++ modules/matka.py
@@ -2,14 +2,8 @@
 matka.py
 Made by rolle
 """
-from __future__ import unicode_literals
-import os
-import locale
-os.environ["PYTHONIOENCODING"] = "utf-8"
-scriptLocale=locale.setlocale(category=locale.LC_ALL, locale="fi_FI.UTF-8")
 import sopel.module
 from urllib.request import urlopen
-from smartencoding import smart_unicode, smart_unicode_with_replace
 import json
 
 @sopel.module.example('!matka Helsinki Riihimäki')
