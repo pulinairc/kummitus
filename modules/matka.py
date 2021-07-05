8c830d7abc14d7455ddb282cfb4cd844dceaf122
Roni Laukkarinen <roni@dude.fi>
2021-07-05 15:13:08 +0300

    Add smart_str()

--- modules/matka.py
+++ modules/matka.py
@@ -4,6 +4,7 @@ Made by rolle
 """
 import sopel.module
 from urllib.request import urlopen
+from django.utils.encoding import smart_str, smart_unicode
 import json
 
 url = 'https://www.vaelimatka.org/route.json?stops=%s|%s'
@@ -17,6 +18,6 @@ def module(bot, trigger):
     if not start or not end:
         bot.reply('Tarvitaan lähtö- ja saapumispaikat')
     else:
-        response = urlopen(url % (start.strip(), end.strip()))
+        response = urlopen(url % (smart_str(start), smart_str(end)))
         data_json = json.loads(response.read())
         bot.reply(data_json["distance"])
