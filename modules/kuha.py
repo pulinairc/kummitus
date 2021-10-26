6dd47d382014731df35f5ea2f7874a3ad13a4242
Roni Laukkarinen <roni@dude.fi>
2021-10-26 10:57:57 +0300

    Fix kuha

--- modules/kuha.py
+++ modules/kuha.py
@@ -1,27 +1,11 @@
 """
-kuha.py - Sopel Lannistaja Kuha Module
-Copyright 2015, Marcus Leivo <meicceli@sopel.mail.kapsi.fi>
-Licensed under the Eiffel Forum License 2.
-http://sopel.chat/
+kuha.py
+Lannistajakuha.com
+Sopel module made by rolle
 """
-from urllib.request import urlopen
-from bs4 import BeautifulSoup
-from sopel.module import commands, example
 
-@commands('kuha')
-@example('.kuha 69')
-def lannistaja_kuha(bot, trigger):
-    number = ""
-    if trigger.group(2):
-        try:
-            number = str(int(trigger.group(2).split(" ")[0]))
-        except:
-            pass
-    if len(number) > 0:
-        url = urlopen("http://lannistajakuha.com/" + number).read().decode()
-    else:
-        url = urlopen("http://lannistajakuha.com/random").read().decode()
-    soup = BeautifulSoup(url, features = "lxml")
-    result = (soup.find("p", attrs={"class": "teksti"}))
-    result = result.text.strip()
-    bot.say(result)
+@commands('kuha', 'lannistajakuha')
+
+def kuha(bot, trigger):
+    moti = random.choice(list(open('/home/rolle/.sopel/modules/kuhat.txt')))
+    bot.say(kuha)
