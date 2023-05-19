9b4e25921080f2c1a8a5ac1386d97b0a285eb0fd
Roni Laukkarinen <roni@dude.fi>
2023-05-19 17:26:45 +0300

    Rewrite weather for foreca

--- modules/suomensaa.py
+++ modules/suomensaa.py
@@ -1,7 +1,7 @@
 """
 suomensaa.py
 Made by rolle
-Updated 20200628
+Updated 2023-05-19
 """
 import sopel.module
 from urllib.request import urlopen
@@ -77,50 +77,33 @@ def saa(bot, trigger):
   if not place:
     place = trigger.group(2).strip()
 
-  url = "https://yle.fi/saa/suomi/%s" % place    
-  url_sunsetsunrise = "http://www.moisio.fi/taivas/aurinko.php?paikka=%s" % place    
+  # Readable version of the place
+  place_readable = trigger.group(2).strip()
+
+  # Change ä to a and ö to o
+  place = place.replace('ä', 'a')
+  place = place.replace('ö', 'o')
+
+  url_ampparit = "https://www.ampparit.com/saa/%s" % place
+  url_foreca = "https://www.foreca.fi/Finland/%s" % place
+  url_moisio = "http://www.moisio.fi/taivas/aurinko.php?paikka=%s" % place
 
   # LXML Xpath based scraping
-  r = requests.get(url, headers={'Accept-Language': 'fi-FI', 'Content-type': 'text/html;charset=UTF-8', "accept-encoding": "gzip, deflate"})
+  r = requests.get(url_ampparit, headers={'Accept-Language': 'fi-FI', 'Content-type': 'text/html;charset=UTF-8', "accept-encoding": "gzip, deflate"})
   r.encoding == 'ISO-8859-1' and not 'ISO-8859-1' in r.headers.get('Content-Type', '')
-  root = lxml.html.fromstring(r.content)
+  ampparit = lxml.html.fromstring(r.content)
 
-  if place == 'jyväskylä':
-    url_jkl = "http://weather.jyu.fi"
-    r_jkl = requests.get(url_jkl)
-    root_jkl = lxml.html.fromstring(r_jkl.content)
+  # Get for textdata
+  r_textdata = requests.get(url_foreca, headers={'Accept-Language': 'fi-FI', 'Content-type': 'text/html;charset=UTF-8', "accept-encoding": "gzip, deflate"})
+  r_textdata.encoding == 'ISO-8859-1' and not 'ISO-8859-1' in r_textdata.headers.get('Content-Type', '')
+  foreca = lxml.html.fromstring(r_textdata.content)
 
-    #try:
+  # Get for sunsetsunrise
+  r_sunsetsunrise = requests.get(url_moisio, headers={'Accept-Language': 'fi-FI', 'Content-type': 'text/html;charset=UTF-8', "accept-encoding": "gzip, deflate"})
+  r_sunsetsunrise.encoding == 'ISO-8859-1' and not 'ISO-8859-1' in r_sunsetsunrise.headers.get('Content-Type', '')
+  moisio = lxml.html.fromstring(r_sunsetsunrise.content)
 
-    # http://weather.jyu.fi/
-    place_exact_get = root_jkl.xpath('//*[@id="content"]/fieldset/legend')
-    place_exact = place_exact_get[0].text.strip()
-    temperature_get = root_jkl.xpath('//*[@id="table-a"]/tbody/tr[1]/td[2]')
-    temperature = temperature_get[0].text.strip()
-    time_get = root_jkl.xpath('//*[@id="c2"]/text()')
-
-    # Nämä samat kuin elsen jälkeen
-    rain_probability_get = root.xpath('//*[@id="pointdata-container"]/div[2]/div[2]/div[1]/span/span[1]/span[3]')
-    temperature_nextday_get = root.xpath('//*[@id="dailydata"]/li[1]/span[2]/span[5]/span[3]/span[1]/span[2]')
-    textweather_get = root.xpath('//*[@id="pointdata-container"]/div[2]/div[1]/div[1]/span/span')
-    textweather = textweather_get[0].text.strip().encode('latin-1').decode(encoding='utf-8',errors='strict')
-    nextday_text_get = root.xpath('//*[@id="dailydata"]/li[1]/span[2]/span[5]/span[2]/span/span')
-    feelslike_get = root.xpath('//*[@id="pointdata-container"]/div[2]/div[1]/div[2]/span/text()')
-    precipitation_amount_get = root.xpath('//*[@id="pointdata-container"]/div[2]/div[2]/div[1]/span/span[1]/span[5]/strong')
-    temperature_nextday = temperature_nextday_get[0].text.strip()
-    rain_probability = rain_probability_get[0].text.strip()
-    nextday_text = nextday_text_get[0].text.strip().encode('latin-1').decode(encoding='utf-8',errors='strict')
-    textweather = textweather_get[0].text.strip().encode('latin-1').decode(encoding='utf-8',errors='strict')
-    time = time_get[0]
-    feelslike = feelslike_get[0]
-    precipitation_amount = precipitation_amount_get[0].text.strip()
-
-    bot.say('\x02Jyväskylä, ' + place_exact + '\x0F: ' + temperature + ' (' + textweather + ', ' + feelslike + '), mitattu ' + time + '. Sateen todennäköisyys: ' + rain_probability + '%, määrä: ' + precipitation_amount + '. Huomispäiväksi luvattu ' + temperature_nextday + ' (' + nextday_text + ').')
-
-    #except:
-      #bot.say('Error, tilt, nyt bugaa! Sijainnin \x02' + place.capitalize() + '\x0F säätä ei saatu haettua. Heitä ihmeessä pull requestia, jos tiedät miten tämä korjataan. Sään tarjoilee: https://github.com/pulinairc/kummitus/blob/master/modules/suomensaa.py')
-
-  elif place == 'rolle':
+  if place == 'rolle':
 
     url_rolle = "https://c.rolle.wtf/raw.php"
     temps = urlopen(url_rolle).read().decode("utf-8")
@@ -129,43 +112,69 @@ def saa(bot, trigger):
   else:
 
     try:
-      # Pääinputin value
-      city = root.xpath('//*[@id="searchField"]/@value')[0].encode('latin-1').decode(encoding='utf-8',errors='strict')
+      # NB! First, disable JS
+      # If JS is disabled and nothing is found, they have changed it and it won't work. You'll have to find another page.
 
-      # "Säätila tänään klo XX:XX" XPath
-      time_get = root.xpath('//*[@id="pointdata-container"]/div[1]/h3')
+      # Main title: "Sää Helsinki | 10 vrk sää"
+      city_get = ampparit.xpath('//*[@id="content"]/div[1]/h1')
 
-      # "Sää tulevina päivinä", seuraavan päivän kello 16 kohdalla olevan lämpötiladivin XPath
-      temperature_nextday_get = root.xpath('//*[@id="dailydata"]/li[1]/span[2]/span[5]/span[3]/span[1]/span[2]')
+      # Split the city name from title
+      city = city_get[0].text.strip().split('|')[0].replace('Sää ', '')
 
-      # Sateen todennäköisyys
-      rain_probability_get = root.xpath('//*[@id="pointdata-container"]/div[2]/div[2]/div[1]/span/span[1]/span[3]')
+      # In the "Klo" column, first number, weather-time class, custom built XPath for reliability
+      time_get = ampparit.xpath('//*[@class="weather-hour"]/div[@class="weather-time"]/time')
+      time = time_get[0].text.strip()
 
-      # "Sää tulevina päivinä", seuraavan päivän kello 16 kohdalla olevan kuvan sisällä olevan spanin XPath
-      nextday_text_get = root.xpath('//*[@id="dailydata"]/li[1]/span[2]/span[5]/span[2]/span/span')
-
-      # XPath: https://dsh.re/accc8
-      temperature_get = root.xpath('//*[@id="pointdata-container"]/div[2]/div[1]/div[2]/div/span[2]')
-
-      # XPath: https://dsh.re/82f509
-      feelslike_get = root.xpath('//*[@id="pointdata-container"]/div[2]/div[1]/div[2]/span/text()')
-
-      # XPath: https://dsh.re/0c95b
-      textweather_get = root.xpath('//*[@id="pointdata-container"]/div[2]/div[1]/div[1]/span/span')
-
-      # Sademäärä-ennuste
-      precipitation_amount_get = root.xpath('//*[@id="pointdata-container"]/div[2]/div[2]/div[1]/span/span[1]/span[5]/strong')
-
-      temperature_nextday = temperature_nextday_get[0].text.strip()
-      rain_probability = rain_probability_get[0].text.strip()
-      nextday_text = nextday_text_get[0].text.strip().encode('latin-1').decode(encoding='utf-8',errors='strict')
+      # Temperature, class weather-temperature under the symbol
+      temperature_get = ampparit.xpath('//*[@id="content"]/div[2]/div[2]/div/div/div[1]/div[2]')
       temperature = temperature_get[0].text.strip()
-      textweather = textweather_get[0].text.strip().encode('latin-1').decode(encoding='utf-8',errors='strict')
-      time = time_get[0].text.strip().encode('latin-1').decode(encoding='utf-8',errors='strict')
-      feelslike = feelslike_get[0]
-      precipitation_amount = precipitation_amount_get[0].text.strip()
 
-      bot.say('\x02' + city.capitalize() + '\x0F - ' + time + ': ' + temperature + ' (' + textweather + ', ' + feelslike + '). Sateen todennäköisyys: ' + rain_probability + '%, määrä: ' + precipitation_amount + '. Huomispäiväksi luvattu ' + temperature_nextday + ' (' + nextday_text + ').')
+      # Max temperature, red number under "Ylin:"
+      temperature_max_get = foreca.xpath('//*[@id="dailybox"]/div[1]/a/div/p[2]/abbr')
+      temperature_max = temperature_max_get[0].text.strip()
+
+      # Min temperature, class weather-min-temperature under the symbol
+      temperature_min_get = ampparit.xpath('//*[@id="content"]/div[2]/div[2]/div/div/div[1]/div[3]')
+      temperature_min = temperature_min_get[0].text.strip().replace('Alin: ', '')
+
+      # Text version of the weather today
+      text_weather_today_get = foreca.xpath('//*[@class="txt"]')
+      text_weather_today = text_weather_today_get[0].text.strip().split('.')[0]
+
+      # Feels like, class weather-temperature-feelslike in the "Lämpö (Tuntuu)" column, first row
+      feelslike_get = ampparit.xpath('//*[@id="content"]/div[3]/div[1]/div/div/div[2]/div[3]/span[2]')
+      feelslike = feelslike_get[0].text.strip().replace('(', '').replace(')', '')
+
+      # Rain
+      rain_get = ampparit.xpath('//*[@id="content"]/div[2]/div[2]/div/div/div[1]/div[4]/text()')
+      rain = rain_get[0]
+
+      # Sun rises
+      sun_rises_get = moisio.xpath('//td[@class="tbl0"][4]')
+      sun_rises = sun_rises_get[0].text.strip()
+
+      # Sun sets
+      sun_sets_get = moisio.xpath('//td[@class="tbl0"][5]')
+      sun_sets = sun_sets_get[0].text.strip()
+
+      # Day lenght
+      day_length_get = moisio.xpath('//td[@class="tbl0"][6]')
+      day_length = day_length_get[0].text.strip()
+
+      # Text version of the weather today
+      text_weather_tomorrow_title_get = foreca.xpath('//*[@class="txt"]')
+      text_tomorrow_today = text_weather_tomorrow_title_get[1].text.strip().split('.')[0]
+
+      # Temperature for tomorrow
+      temperature_tomorrow_get = ampparit.xpath('//*[@class="weather-temperature"]')
+      temperature_tomorrow = temperature_tomorrow_get[1].text.strip()
+
+      # Min temperature in the "Huomenna" column, celsius under the symbol
+      temperature_nextday_min_get = ampparit.xpath('//*[@id="content"]/div[2]/div[2]/div/div/div[2]/div[3]')
+      temperature_nextday_min = temperature_nextday_min_get[0].text.strip()
+
+      # Say it all out loud
+      bot.say('\x02' + city.capitalize() + '\x0Fklo ' + time + ':00: \x02' + temperature + '\x0F, ' + text_weather_today.lower() + '\x0F (tuntuu kuin: ' + feelslike + '). Sadetta mahdollisesti\x02' + rain + '\x0F. Kuluvan päivän ylin lämpötila: \x02' + temperature_max + '\x0F, alin: \x02' + temperature_min + '\x0F. Aurinko nousee klo \x02' + sun_rises + '\x02 ja laskee klo \x02' + sun_sets + '\x02. Päivän pituus on \x02' + day_length + '\x02. Huomiseksi luvassa on \x02' + temperature_tomorrow + '\x0F, ' + text_tomorrow_today.lower() + ' (kylmin lämpötila huomenna: \x02' + temperature_nextday_min + '\x0F).')
 
     except:
-      bot.say('Error, tilt, nyt bugaa! Sijainnin \x02' + place.capitalize() + '\x0F säätä ei saatu haettua. Heitä ihmeessä pull requestia, jos tiedät miten tämä korjataan. Sään tarjoilee: https://github.com/pulinairc/kummitus/blob/master/modules/suomensaa.py')
+      bot.say('Error, tilt, nyt bugaa! Sijainnin \x02' + place_readable.capitalize() + '\x0F säätä ei saatu haettua. Heitä ihmeessä pull requestia, jos tiedät miten tämä korjataan. Sään tarjoilee: https://github.com/pulinairc/kummitus/blob/master/modules/suomensaa.py')
