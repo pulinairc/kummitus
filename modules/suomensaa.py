581aa94ac3df9c1084c056a1d844f90fe3d6b214
Roni Laukkarinen <roni@dude.fi>
2023-07-16 17:59:24 +0300

    Attempt to fix providers

--- modules/suomensaa.py
+++ modules/suomensaa.py
@@ -20,15 +20,20 @@ import datetime
 import os
 import json
 
+# Add /home/rolle/sopel-weather/build/lib/sopel_modules to path
+import sys
+sys.path.insert(0, '/home/rolle/sopel-weather/build/lib/sopel_modules/weather/providers/weather')
+
 from sopel.config.types import NO_DEFAULT, ChoiceAttribute, StaticSection, ValidatedAttribute
 from sopel.module import commands, example, NOLIMIT
 from sopel.modules.units import c_to_f
 from sopel.tools import Identifier
 from sopel.tools.time import format_time
 
-from sopel.modules.weather.providers import openmeteo_forecast, openmeteo_weather
-from sopel.modules.weather.providers import openweathermap_forecast, openweathermap_weather
-from sopel.modules.weather.providers import pirateweather_forecast, pirateweather_weather
+# Import our providers
+from .openmeteo import openmeteo_forecast, openmeteo_weather
+from .openweathermap import openweathermap_forecast, openweathermap_weather
+from .pirateweather import pirateweather_forecast, pirateweather_weather
 
 WEATHER_PROVIDERS = [
     'openmeteo',
