b73db9074863eb90ff83f2d6b513999e2dd574c5
Roni Laukkarinen <roni@dude.fi>
2023-07-16 16:55:29 +0300

    Test

--- modules/autokick.py
+++ modules/autokick.py
@@ -6,11 +6,11 @@ A module for the Sopel IRC Bots.
 """
 import sopel.module
 
-# Define an array of words in python
-words = ['rumasana']
-
 # Define bot, trigger all messages
 def kickbot(bot, trigger):
+
+    # Define an array of words in python
+    words = ['rumasana']
 
     # Check if any of the words are in the message
     if any(word in trigger for word in words):
